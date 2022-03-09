659af38a43f764ffb6c9649c057eeb68b3c97231
Sunmin Choi <sunminchoi1214@gmail.com>
2022-03-09 00:29:27 -0800

    Added move generation for three marble moves.

--- state_space_generator.py
+++ state_space_generator.py
@@ -38,27 +38,117 @@ class StateSpaceGenerator:
                 if marble1.node_value == marble2.node_value}
 
     def is_valid_two_marble_move(self, move):
-        new_start_node = self.get_node_in_direction_of_node(move.start_node, move.direction)  # location of start node after move
-        new_end_node = self.get_node_in_direction_of_node(move.end_node, move.direction)  # location of end node after move
+        result_start_node = self.get_node_in_direction_of_node(move.start_node,
+                                                               move.direction)  # location of start node after move
+        result_end_node = self.get_node_in_direction_of_node(move.end_node,
+                                                             move.direction)  # location of end node after move
         if move.is_inline_move():
-            front_node = new_start_node if new_end_node == move.start_node else new_end_node
-            # front_node is the node that potentially will be pushed, so if A1-B1-TL, then it would be C1
-            if front_node.node_value == NodeValue.INVALID:  # edge of board
+            # push_node is the node that potentially will be pushed, so if A1-B1-TL, then it would be C1
+            push_node = result_start_node if result_end_node == move.start_node else result_end_node
+
+            if push_node.node_value == NodeValue.INVALID:  # edge of board
                 return False
-            if front_node.node_value == NodeValue.EMPTY:
+            if push_node.node_value == NodeValue.EMPTY:
                 return True
-            elif (front_node.node_value.value == abs(3 - move.start_node.node_value.value)) & \
-                    (self.get_node_in_direction_of_node(front_node, move.direction).node_value == NodeValue.EMPTY):
-                return True
+
+            # if node being pushed is opposite colour
+            if push_node.node_value.value == abs(3 - move.start_node.node_value.value):
+
+                # if node behind pushed node is empty or off the board
+                if (self.get_node_in_direction_of_node(push_node, move.direction).node_value == NodeValue.INVALID) | \
+                        (self.get_node_in_direction_of_node(push_node, move.direction).node_value == NodeValue.EMPTY):
+                    return True
+
+        # sidestep move
         else:
-            if (new_start_node.node_value == NodeValue.EMPTY) & \
-                    (new_end_node.node_value == NodeValue.EMPTY):
+            if (result_start_node.node_value == NodeValue.EMPTY) & \
+                    (result_end_node.node_value == NodeValue.EMPTY):
+                return True
+        return False
+
+    def get_valid_three_marble_selections(self):
+        """
+        Returns a set of tuples of nodes (Node1, Node2) for all valid 3 marble selections
+        for the current player.
+        :return: a set of tuples of nodes, the valid selections for three marbles
+        """
+        valid_marble_selections = set()
+        curr_player_marbles = self.state_rep.get_all_marbles_for_player(self.state_rep.player)
+        for marble1 in curr_player_marbles:
+            for direction in Direction.left_directions():
+                marble2 = self.get_node_in_direction_of_node(marble1, direction)
+                if marble2.node_value is not NodeValue.INVALID:
+                    marble3 = self.get_node_in_direction_of_node(marble2, direction)
+                    if (marble1.node_value == marble2.node_value) & (marble1.node_value == marble3.node_value):
+                        valid_marble_selections.add((marble1, marble3))
+        return valid_marble_selections
+
+    def generate_three_marble_moves(self):
+        marble_selections = self.get_valid_three_marble_selections()
+        all_moves = {Move(node1, node2, direction) for (node1, node2) in marble_selections for direction in Direction}
+        valid_moves = {move for move in all_moves if self.is_valid_three_marble_move(move)}
+        return valid_moves
+
+    def is_valid_three_marble_move(self, move):
+        result_start_node = self.get_node_in_direction_of_node(move.start_node, move.direction)  # location of start node after move
+        result_end_node = self.get_node_in_direction_of_node(move.end_node, move.direction)  # location of end node after move
+
+        middle_node = None
+        for direction in Direction:
+            if self.get_node_in_opposite_direction_of_node(move.end_node, direction) == \
+                    self.get_node_in_direction_of_node(move.start_node, direction):
+                middle_node = self.get_node_in_direction_of_node(move.start_node, direction)
+
+        result_middle_node = self.get_node_in_direction_of_node(middle_node, move.direction)
+
+        if move.is_inline_move():
+            # push_node is the node that potentially will be pushed, so if A1-C1-TL, then it would be D1
+            if result_middle_node == move.start_node:
+                push_node = result_start_node
+            else:
+                push_node = result_end_node
+
+            if push_node.node_value == NodeValue.INVALID:  # edge of board
+                return False
+            if push_node.node_value == NodeValue.EMPTY:
+                return True
+
+            # if node being pushed is opposite colour
+            if push_node.node_value.value == abs(3 - move.start_node.node_value.value):
+
+                # if node behind pushed node is empty or off the board (3 pushing 1)
+                if (self.get_node_in_direction_of_node(push_node, move.direction).node_value == NodeValue.INVALID) | \
+                        (self.get_node_in_direction_of_node(push_node, move.direction).node_value == NodeValue.EMPTY):
+                    return True
+
+                behind_push_node = self.get_node_in_direction_of_node(push_node, move.direction)
+
+                # if node behind pushed node is also opposite colour
+                if behind_push_node.node_value.value == abs(3 - move.start_node.node_value.value):
+
+                    # if node behind pushed node is empty or off the board (3 pushing 2)
+                    if (self.get_node_in_direction_of_node(behind_push_node, move.direction).node_value
+                        == NodeValue.INVALID) | \
+                            (self.get_node_in_direction_of_node(behind_push_node, move.direction).node_value
+                             == NodeValue.EMPTY):
+                        return True
+
+        # sidestep move
+        else:
+            if (result_start_node.node_value == NodeValue.EMPTY) & \
+                    (result_end_node.node_value == NodeValue.EMPTY) & \
+                    (result_middle_node.node_value == NodeValue.EMPTY):
                 return True
         return False
 
     def get_node_in_direction_of_node(self, node, direction):
         result_row = node.row + direction.value[0][0]
         result_column = node.column + direction.value[0][1]
+        return self.state_rep.get_node(result_row, result_column)
+
+    def get_node_in_opposite_direction_of_node(self, node, direction):
+        result_row = node.row - direction.value[0][0]
+        result_column = node.column - direction.value[0][1]
         return self.state_rep.get_node(result_row, result_column)
 
     def get_adjacent_nodes_values(self, node):
@@ -74,7 +164,7 @@ class StateSpaceGenerator:
     def get_left_nodes(self, node):
         """
         Returns a set with the adjacent nodes in left directions (TL, L, BL).
-        Used when generating selections of 2 or 3 marbles in a row.
+        Used when generating selections of 2 marbles in a row.
         :param node: a Node
         :return: set of Nodes in the left directions
         """
@@ -88,3 +178,5 @@ if __name__ == "__main__":
     print(stateSpaceGen.generate_one_marble_moves())  # implement a way to sort move outputs alphabetically?
     print(stateSpaceGen.get_valid_two_marble_selections())
     print(stateSpaceGen.generate_two_marble_moves())
+    print(stateSpaceGen.get_valid_three_marble_selections())
+    print(stateSpaceGen.generate_three_marble_moves())
